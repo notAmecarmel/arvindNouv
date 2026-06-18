c7d546ea11248bc2f4e9375f24f536d1efe5e5af
Mac <mac@Macs-Air.bbrouter>
2026-06-18 18:17:31 +0530

    debugged contact.js

--- api/contact.js
+++ api/contact.js
@@ -1,5 +1,8 @@
 export default async function handler(req, res) {
 
+  console.log("REQUEST METHOD:", req.method);
+  console.log("REQUEST BODY:", req.body);
+
   if (req.method !== "POST") {
     return res.status(405).json({
       success: false
@@ -7,6 +10,8 @@ export default async function handler(req, res) {
   }
 
   try {
+
+    console.log("About to call Apps Script");
 
     const response = await fetch(
       "https://script.google.com/macros/s/AKfycbyTMVmxaDtp7w3wzGc1Gn0eT796RnZ1wY-ZEv1GFFwc5AiDgxpPyzOIWP-Ly4cXkYqE/exec",
@@ -19,15 +24,23 @@ export default async function handler(req, res) {
       }
     );
 
+    console.log("Apps Script status:", response.status);
+
+    const text = await response.text();
+
+    console.log("Apps Script response:", text);
+
     return res.status(200).json({
       success: true
     });
 
   } catch (error) {
 
+    console.error("FULL ERROR:", error);
+
     return res.status(500).json({
       success: false,
-      error: error.message
+      message: error.message
     });
 
   }
